7624f7479a15cc5326b7709787d4009661ccede5
Parth Kikani <parthkikani02@gmail.com>
2024-10-09 11:50:00 +0300

    add tab navigation

---
+++ src/screens/CategoriesScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const CategoriesScreen = () => {
+  return <Text>CategoriesScreen</Text>;
+};
+
+export default CategoriesScreen;
---
+++ src/screens/TransactionsScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const TransactionsScreen = () => {
+  return <Text>TransactionsScreen</Text>;
+};
+
+export default TransactionsScreen;
