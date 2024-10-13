79aa70714fc1991dcbc0340b5688e70270e741b2
Parth Kikani <parthkikani02@gmail.com>
2024-10-13 12:59:35 +0300

    modify pie chart

--- src/components/CustomPieChart.js
+++ src/components/CustomPieChart.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
 import { PieChart } from 'react-native-svg-charts';
-import { G, Image, Text as SvgText, Circle } from 'react-native-svg';
+import { G, Image, Text as SvgText } from 'react-native-svg';
 import foodIcon from '../../assets/food.png';
 import houseIcon from '../../assets/house.png';
 import airplaneIcon from '../../assets/airplane.png';
 import shoppingIcon from '../../assets/cart.png';
 
 const CustomPieChart = () => {
-  const data = [
-    { key: 1, amount: 120, svg: { fill: '#FFCC00' } }, // yellow
-    { key: 2, amount: 80, svg: { fill: '#66BB6A' } }, // green
-    { key: 3, amount: 60, svg: { fill: '#42A5F5' } }, // blue
-    { key: 4, amount: 220, svg: { fill: '#FFA726' } }, // orange
+  const monthlyEmission = [
+    { key: 1, amount: 150, svg: { fill: '#FFCC00' } },
+    { key: 2, amount: 195, svg: { fill: '#66BB6A' } },
+    { key: 3, amount: 100, svg: { fill: '#42A5F5' } },
+    { key: 4, amount: 200, svg: { fill: '#FFA726' } },
   ];
 
-  const imageIcons = [foodIcon, houseIcon, airplaneIcon, shoppingIcon];
+  const imageIcons = [airplaneIcon, foodIcon, shoppingIcon, houseIcon];
 
   const Labels = ({ slices }) => {
     return slices.map((slice, index) => {
-      const { labelCentroid, pieCentroid, data } = slice;
-      // Corrected centering of the icon using pieCentroid
+      const { pieCentroid } = slice;
       const iconSize = 15;
       return (
         <G key={index} x={pieCentroid[0]} y={pieCentroid[1]}>
@@ -45,12 +43,19 @@ const CustomPieChart = () => {
         <PieChart
           style={{ height: '100%' }}
           valueAccessor={({ item }) => item.amount}
-          data={data}
+          data={monthlyEmission}
           spacing={0}
           innerRadius={'80%'}
         >
           <Labels />
         </PieChart>
+
+        {/* Centered Text */}
+        <View style={styles.centeredTextContainer}>
+          <Text style={styles.centeredTextSmall}>THIS MONTH</Text>
+          <Text style={styles.centeredTextLarge}>480</Text>
+          <Text style={styles.centeredTextSmall}>Kg CO2e</Text>
+        </View>
       </View>
 
       {/* Average CO2 Info */}
@@ -73,6 +78,31 @@ const styles = StyleSheet.create({
     // borderWidth: 1,
     height: 200,
     width: 300,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  centeredTextContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+    // borderWidth: 1,
+    alignSelf: 'center',
+    top: 60,
+  },
+  centeredTextSmall: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  centeredTextLarge: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold',
+    margin: 5,
+  },
+  centeredText: {
+    position: 'absolute',
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: 'bold',
   },
   averageText: {
     color: '#fff',
